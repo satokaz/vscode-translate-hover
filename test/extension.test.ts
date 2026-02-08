445bd706f3d55ec78c43ed0c577be4ed1c0e7396
satokaz <kazus.sunfish@gmail.com>
2026-02-08 12:27:26 +0900

    feat: add language detection utilities and corresponding tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import { AUTO_DETECT_PAIRS } from '../src/constants';
 import { buildGoogleTranslateUrl } from '../src/providers/google';
 import { formatTranslationResult } from '../src/utils/format';
-import { resolveTargetLanguage } from '../src/utils/languageDetector';
+import { detectLanguage, isChinese, isJapanese, isKorean, resolveTargetLanguage } from '../src/utils/languageDetector';
 
 suite('Utils Tests', () => {
     test('formatTranslationResult adds spacing around fullwidth parentheses', () => {
@@ -33,5 +33,27 @@ suite('Utils Tests', () => {
         assert.strictEqual(params.get('q'), 'Hello world');
         assert.ok(params.getAll('dt').includes('t'));
         assert.ok(params.getAll('dt').includes('bd'));
+    });
+
+    test('isJapanese detects Japanese text by ratio', () => {
+        assert.strictEqual(isJapanese('\u3042\u3044\u3046abc'), true);
+        assert.strictEqual(isJapanese('abcdef'), false);
+    });
+
+    test('isChinese detects Chinese text and excludes Japanese-only scripts', () => {
+        assert.strictEqual(isChinese('\u4E2D\u6587abc'), true);
+        assert.strictEqual(isChinese('\u3042\u3044\u4E2D'), false);
+    });
+
+    test('isKorean detects Korean text by ratio', () => {
+        assert.strictEqual(isKorean('\uAC00\uB098\uB2E4abc'), true);
+        assert.strictEqual(isKorean('abc123'), false);
+    });
+
+    test('detectLanguage returns expected language codes', () => {
+        assert.strictEqual(detectLanguage('\u3042\u3044\u3046'), 'ja');
+        assert.strictEqual(detectLanguage('\u4E2D\u6587\u4E2D'), 'ja');
+        assert.strictEqual(detectLanguage('\uAC00\uB098\uB2E4'), 'ko');
+        assert.strictEqual(detectLanguage('Hello'), 'en');
     });
 });
